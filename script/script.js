77c4373d3d16d9724542f04642f31c86afc7451f
Akshay200228 <sankpla2812@gmail.com>
2023-01-03 16:41:02 +0530

    first commit

---
+++ script/script.js
@@ -0,0 +1,30 @@
+let url = "https://kontests.net/api/v1/all"
+
+
+fetch(url).then((res) => { return res.json() }).then((val) => {
+  // console.log(val)
+  let ran1 = Math.floor(Math.random() * 47);
+  let ran2 = Math.floor(Math.random() * 47);
+  let ran3 = Math.floor(Math.random() * 47);
+  let ran4 = Math.floor(Math.random() * 47);
+  let ran5 = Math.floor(Math.random() * 47);
+  let ran6 = Math.floor(Math.random() * 47);
+
+
+  const recurence =  (name,site,start,end,status,url,ran)=>{
+    name.innerHTML=val[ran].name
+    site.innerHTML=val[ran].site
+    start.innerHTML=val[ran].start_time
+    end.innerHTML=val[ran].end_time
+    status.innerHTML=val[ran].status
+    url.href=val[ran].url
+  }
+
+  recurence(firstC,SiteName1,Start1,End1,Status1,url1,ran1)
+  recurence(secondC,SiteName2,Start2,End2,Status2,url2,ran2)
+  recurence(thirdC,SiteName3,Start3,End3,Status3,url3,ran3)
+  recurence(fourthC,SiteName4,Start4,End4,Status4,url4,ran4)
+  recurence(fiveC,SiteName5,Start5,End5,Status5,url5,ran5)
+  recurence(sixC,SiteName6,Start6,End6,Status6,url6,ran6)
+
+})
